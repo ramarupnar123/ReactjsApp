0c007c59194e06e9bf62277de1a896efde623fb0
sanjaydas <sdas69101@gmail.com>
2025-01-07 14:39:29 +0530

    added position sticky for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
       <div className="container">
         <Link className="navbar-brand" to="/">
           BlogApp
